146ebed0f2e43d1aca4592085729e20065010a18
gulfurkan <furkangul0519@gmail.com>
2021-12-27 22:10:28 +0300

    Changing connection method for jdk 1.8 and adding auth option

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 //JSON simple library imports
 import org.json.simple.JSONArray;
@@ -13,61 +15,70 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class App {
+class App {
+    private int _counter=0;
+    private StringBuilder _processBar = new StringBuilder("_________________________");
+    private String _token;
 
-    static JSONObject getJSONObject(String s) throws ParseException {
+    App(String token){
+        this._token=token;
+    }
+
+    private HttpURLConnection _getConnection(String url) throws MalformedURLException, IOException{     
+        HttpURLConnection httpClient = (HttpURLConnection) new URL(url).openConnection();        
+        httpClient.setRequestProperty("Authorization", "Bearer "+this._token);
+        httpClient.setRequestMethod("GET");
+        return httpClient;
+    }
+
+    private JSONObject _getJSONObject(String s) throws ParseException {
         JSONParser parser = new JSONParser();
         return (JSONObject) parser.parse(s);
     }
 
-    static JSONArray getJSONArray(String s) throws ParseException {
+    private JSONArray _getJSONArray(String s) throws ParseException {
         JSONParser parser = new JSONParser();
         return (JSONArray) parser.parse(s);
     }
 
-    static String getJSONString(String url) throws IOException, ParseException {
-        try {
+    private String _getResponseString(String url) throws IOException {
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
+        //Getting connection
+        HttpURLConnection httpClient = _getConnection(url);
 
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+        if(httpClient.getResponseCode()!=200)
+            return null;
 
-            return response.body();
-
-        } catch (
-
-        Exception e) {
-            e.printStackTrace();
+        //Creating a string from response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
         }
-        return null;
+
+        return response.toString();
     }
 
-    static List<User> getContributers(JSONArray contributersJson) throws IOException, ParseException {
+    private List<User> _getContributers(JSONArray contributersJson) throws IOException, ParseException {
 
         List<User> contributers = new ArrayList<>();
+        
 
-        for (int i = 0; i < contributersJson.size(); i++) {
-
-            // Limiting contributers list
-            if (i == 10)
-                break;
+        for (int i = 0; i < Math.min(contributersJson.size(), 10); i++) {
 
             // Gettting JSON object from JSONArray
             JSONObject contributer = (JSONObject) contributersJson.get(i);
 
             // Getting JSON string for every user
-            String jsonString = getJSONString(contributer.get("url").toString());
+            String jsonString = _getResponseString(contributer.get("url").toString());
 
             if (jsonString == null) {
                 break;
             }
-
+            
             // Parsing JSON string to JSON object
-            JSONObject userJson = (JSONObject) getJSONObject(jsonString);
+            JSONObject userJson = (JSONObject) _getJSONObject(jsonString);
 
             // Getting datas from JSON
             String name = userJson.get("login") != null ? userJson.get("login").toString() : " null ";
@@ -79,58 +90,84 @@ public class App {
 
             // Adding to the contributer list
             contributers.add(new User(name, location, company, contributeCount));
+
+            // displaying processbar
+            _processBar.setCharAt(_counter / 2, '*');
+            _counter++;
+            System.out.print("\rCreating contributers.txt file : " + _processBar);
         }
 
         return contributers;
     }
 
-    static void printFile(List<String> list) throws IOException {
-        FileWriter fw = new FileWriter("out.txt");
+    private void _printFile(List<String> list) throws IOException {
+        FileWriter fw = new FileWriter("contributers.txt");
         for (String s : list) {
             fw.write(s + "\n");
         }
         fw.close();
     }
-
-    public static void main(String[] args) throws Exception {
-
+    public void run() throws ParseException{
+        
         final String[] repoNames = { "echarts", "superset", "dubbo", "spark", "airflow" };
         final String url = "https://api.github.com/repos/apache/";
 
         List<Repo> repos = new ArrayList<>();
         List<String> list = new ArrayList<>();
 
+        boolean haveRequest = true;    
+
         try {
+
             for (String repoName : repoNames) {
 
                 // Getting contributers list for every repo as a JSON string
-                String jsonString = getJSONString(url + repoName + "/contributors");
+                String jsonString = _getResponseString(url + repoName + "/contributors");
 
                 if (jsonString == null) {
+                    haveRequest = false;
                     continue;
                 }
-
+                haveRequest = true;
                 // Parsing the string to JSON Array
-                JSONArray contributersJson = (JSONArray) getJSONArray(jsonString);
+                JSONArray contributersJson = (JSONArray) _getJSONArray(jsonString);
 
                 // Getting a list of contributers
-                List<User> contributers = getContributers(contributersJson);
+                List<User> contributers = _getContributers(contributersJson);
 
                 // Adding to repo list
                 repos.add(new Repo(repoName, contributers));
 
                 for (User user : contributers) {
-                    list.add("repo: " + repoName + ", user: " + user.getUserName() +
-                            ", location: " + user.getLocation()
-                            + ", company: " + user.getCompany() + ", contributions: " +
-                            user.getContributes());
+                    list.add("repo: " + repoName
+                            + ", user: " + user.getUserName()
+                            + ", location: " + user.getLocation()
+                            + ", company: " + user.getCompany()
+                            + ", contributions: " + user.getContributes());
                 }
 
                 list.add("");
+                
             }
-            printFile(list);
+
+            if (!haveRequest) {
+                list.add(
+                        "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n You have exceeded the request limit\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+            _printFile(list);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String token = "";
+        System.out.print("Please Enter your token : ");
+        token = sc.nextLine();
+        App app = new App(token);
+        sc.close();
+        app.run();
+    }
+    
 }
